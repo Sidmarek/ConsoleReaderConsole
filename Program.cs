c2a8844dd5897bf6cc20515a0e646348ae0ec191
Marek Novak <chodec.marek@gmail.com>
2020-10-24 21:24:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleReaderConsole
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            bool control = false;
+            while (!control) 
+            {
+                if (Clipboard.ContainsText(TextDataFormat.Text))
+                {
+                    string lastCliboardString = Clipboard.GetText();
+                    if (Uri.IsWellFormedUriString(lastCliboardString, UriKind.Absolute))
+                    {
+                        Console.WriteLine("URL " + Clipboard.GetText());
+                        Process p = new Process();
+                        p.StartInfo.FileName = "youtube-dl.exe";
+                        p.StartInfo.Arguments = lastCliboardString;
+                        p.Start();
+                        control = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Text " + Clipboard.GetText());
+                    }
+                    System.Threading.Thread.Sleep(5000);
+                }
+                else
+                {
+                    Console.WriteLine("no text in clipboard");
+                    System.Threading.Thread.Sleep(5000);
+                }
+
+            }
+        }
+
+    }
+}
